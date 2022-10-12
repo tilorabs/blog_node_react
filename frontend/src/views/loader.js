af53942b431d8e875431ec86c06af5948cbe7c5f
Tilo Rabs <trabs@web.de>
2022-10-12 17:14:47 +0200

    fe_build

--- frontend/src/views/loader.js
+++
@@ -1,9 +0,0 @@
-export default function Loader() {
-    return (
-        <div id="content">
-            <div className="spinner-container">
-                <div className="spinner"></div>
-            </div>
-        </div>
-    )
-}
--- frontend/src/views/search.js
+++
@@ -1,9 +0,0 @@
-function Search({handleSearch, search}) {
-    return (
-        <div className="search" >
-            <input type="text" value={search} placeholder="Search" onChange={({target}) => handleSearch(target.value)}/>
-        </div>
-    );
-}
-
-export default Search;
